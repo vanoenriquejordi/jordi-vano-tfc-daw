121b0dbcf403d0945a6c4a3124f47beb4ea4abd7
Jordi Vañó Enrique <vanoenriquejordi@gmail.com>
2024-03-27 13:31:10 +0100

    Actualizar

--- jordi-vano-tfc-daw/src/App.js
+++ jordi-vano-tfc-daw/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/login"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Acceder
         </a>
       </header>
     </div>
